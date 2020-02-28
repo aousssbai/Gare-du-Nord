9b835e0684d4182fae84eab028226a7b9c10610c
Aouss Sbai <aosbai@cisco.com>
2020-02-28 13:48:00 +0000

    first connection works git add --all

--- tcp_server.py
+++ tcp_server.py
@@ -18,20 +18,20 @@ while True:
     connection, client_address = sock.accept()
 
 
-try:
-    print(f'connection from {client_address}')
+    try:
+        print(f'connection from {client_address}')
 
-    # Receive the data in small chunks and retransmit it
-    while True:
-        data = connection.recv(16)
-        print(f'received {data}')
-        if data:
-            print('sending data back to the client')
-            connection.sendall(data)
-        else:
-            print('no more data from {client_address}')
-            break
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print(f'received {data}')
+            if data:
+                print('sending data back to the client')
+                connection.sendall(data)
+            else:
+                print('no more data from {client_address}')
+                break
         
-finally:
-    # Clean up the connection
-    connection.close()
+    finally:
+        # Clean up the connection
+        connection.close()
