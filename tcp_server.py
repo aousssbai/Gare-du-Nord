e6e730510915741ea608e59b7787b7bcf0e14f6b
Nequo <nequo@github.com>
2020-02-28 13:39:23 +0000

    fix(server): upgrade to python3

--- tcp_server.py
+++ tcp_server.py
@@ -6,7 +6,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('127.0.0.1', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print(f'starting up on {server_address[0]} port {server_address[1]}')
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -14,22 +14,22 @@ sock.listen(1)
 
 while True:
     # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
+    print('waiting for a connection')
     connection, client_address = sock.accept()
 
 
 try:
-    print >>sys.stderr, 'connection from', client_address
+    print(f'connection from {client_address}')
 
     # Receive the data in small chunks and retransmit it
     while True:
         data = connection.recv(16)
-        print >>sys.stderr, 'received "%s"' % data
+        print(f'received {data}')
         if data:
-            print >>sys.stderr, 'sending data back to the client'
+            print('sending data back to the client')
             connection.sendall(data)
         else:
-            print >>sys.stderr, 'no more data from', client_address
+            print('no more data from {client_address}')
             break
         
 finally:
